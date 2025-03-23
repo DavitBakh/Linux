3c06a0708fd75126f86d408f19c17cba6d2f410e
DavitBakh <davobakhshyan@gmail.com>
2025-03-23 23:13:45 +0400

    temp commit

--- TCPServerClient/server.cpp
+++ TCPServerClient/server.cpp
@@ -10,8 +10,56 @@
 #include <pthread.h>
 #include <mutex>
 #include <string>
-#include <vector>
-#include <fcntl.h>
+#include <poll.h>
+
+#define PORT 8888
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void SafeCout(std::string message)
+{
+    pthread_mutex_lock(&mutex);
+    std::cout << message;
+    pthread_mutex_unlock(&mutex);
+}
+
+void* clientHandler(void *arg)
+{
+    char buffer[1001];
+    int client_socket = *((int *)arg);
+    // Receive message from client
+    while (true)
+    {
+        int rs = recv(client_socket, buffer, 1000, 0);
+        if (rs == -1)
+        {
+            SafeCout("client socket connection error");
+            close(client_socket);
+            continue;
+        }
+
+        if (rs == 0)
+        {
+            SafeCout("Client disconnected\n");
+            close(client_socket);
+            break;
+        }
+
+        if (rs > 0)
+        {
+            buffer[rs] = '\0';
+            SafeCout("Got message:\n" + std::string{buffer} + "\n");
+        }
+    }
+
+    close(client_socket);
+    pthread_exit(NULL);
+
+    return NULL;
+}
+
 
 int main()
 {
@@ -24,10 +72,15 @@ int main()
     }
 
     // create an endpoint
+
+    // socket address
     struct sockaddr_in server_address;
+    // internet protocol = AF_INET
     server_address.sin_family = AF_INET;
+    // accept or any address (bind the socket to all available interfaces)
     server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_address.sin_port = htons(9999);
+    // port
+    server_address.sin_port = htons(PORT);
 
     // Bind server_socket to server_address
     if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0)
@@ -37,42 +90,90 @@ int main()
     }
 
     // Listen for incoming connections
-    if (listen(server_socket, 10) < 0)
+    if (listen(server_socket, MAX_CLIENTS) < 0)
     {
         perror("listen failed");
         exit(errno);
     }
     std::cout << "Waiting for connection\n";
 
-    int html_fd = open("index.html", O_RDONLY);
-    char html[4096];
-    int bites = read(html_fd, html, 4096);
-    html[bites] = '\0';
+    struct pollfd fds[MAX_CLIENTS + 1];
+    fds[0].fd = server_socket;
+    fds[0].events = POLLIN;
 
-    char response[4096] = "HTTP/1.1 200 OK\r\n\n";
-    strcat(response, html);
+    for (int i = 1; i <= MAX_CLIENTS; i++)
+        fds[i].fd = -1;
 
     while (true)
     {
-        int client_socket;
-        struct sockaddr_in client_address;
-        unsigned int client_addr_len = sizeof(client_address);
 
-        // Accept incoming connection
-        if ((client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_addr_len)) < 0)
+        int ret = poll(fds, MAX_CLIENTS + 1, -1);
+        if (ret == -1)
         {
-            perror("accept failed");
+            perror("poll failed");
             exit(errno);
         }
 
-        std::cout << "Connected client with address: " << inet_ntoa(client_address.sin_addr) << "\n";
-        send(client_socket, response, sizeof(response), 0);
-        close(client_socket);
-        
+        if (fds[0].revents & POLLIN)
+        {
+            int client_socket;
+            struct sockaddr_in client_address;
+            unsigned int client_addr_len = sizeof(client_address);
+
+            // Accept incoming connection
+            if ((client_socket = accept(server_socket, (struct sockaddr *)&client_address, &client_addr_len)) < 0)
+            {
+                SafeCout("accept failed");
+                exit(errno);
+            }
+
+            SafeCout("Connected client with address: " + std::string{inet_ntoa(client_address.sin_addr)} + "\n");
+
+            for (int i = 1; i <= MAX_CLIENTS; i++)
+            {
+                if (fds[i].fd == -1)
+                {
+                    fds[i].fd = client_socket;
+                    fds[i].events = POLLIN;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 1; i <= MAX_CLIENTS; i++)
+        {
+            if (fds[i].fd != -1 && fds[i].revents & POLLIN)
+            {
+                char buffer[BUFFER_SIZE+1];
+                int rs = recv(fds[i].fd, buffer, BUFFER_SIZE, 0);
+                if (rs == -1)
+                {
+                    SafeCout("client socket connection error");
+                    close(fds[i].fd);
+                    fds[i].fd = -1;
+                    continue;
+                }
+
+                if (rs == 0)
+                {
+                    SafeCout("Client disconnected\n");
+                    close(fds[i].fd);
+                    fds[i].fd = -1;
+                    break;
+                }
+
+                if (rs > 0)
+                {
+                    buffer[rs] = '\0';
+                    SafeCout("Got message:\n" + std::string{buffer} + "\n");
+                }
+            }
+        }
     }
 
     // close
     close(server_socket);
+    pthread_mutex_destroy(&mutex);
 
     return 0;
 }
